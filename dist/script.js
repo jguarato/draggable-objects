f6bb6fe72a9b50925dd6eb1c6237b87e22c2910d
Jessica Guarato <jguaratofs@gmail.com>
2022-09-18 18:22:23 -0300

    Add files via upload

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,92 @@
-const dragSpace = document.querySelector("body");
-var targetElement = dragSpace;
+const dragSpaceTarget = document.querySelector("body");
+let globalTargetElement = dragSpaceTarget;
 
-dragSpace.addEventListener("mousedown", dragStart, false);
-dragSpace.addEventListener("mouseup", dragEnd, false);
-dragSpace.addEventListener("mousemove", dragMotion, false);
+function dragStart(event) {
+    const element = event.target;
+    const draggable = element.dragProperties;
 
-const airplane = document.querySelector("#airplane");
-makeElementDraggable(airplane, 45);
+    if (draggable) {
+        draggable.active = true;
+        if (event.type === "touchstart") {
+            draggable.pointerStart.xPos = event.touches[0].clientX;
+            draggable.pointerStart.yPos = event.touches[0].clientY;
+        } else {
+            draggable.pointerStart.xPos = event.clientX;
+            draggable.pointerStart.yPos = event.clientY;
+        }
+
+        globalTargetElement = element;
+    }
+}
+
+function setTransform(element, xPos = 0, yPos = 0, angle = 0) {
+    element.style.transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+}
+
+function dragMotion(event) {
+    const element = event.target;
+    const draggable = element.dragProperties;
+
+    if (draggable) {
+        const dragMotionActivated =
+            draggable.active &&
+            draggable.pointerStart.xPos &&
+            draggable.pointerStart.yPos;
+
+        if (dragMotionActivated) {
+            event.preventDefault();
+
+            let pointerXOffset = 0;
+            let pointerYOffset = 0;
+
+            if (event.type === "touchmove") {
+                pointerXOffset =
+                    event.touches[0].clientX - draggable.pointerStart.xPos;
+                pointerYOffset =
+                    event.touches[0].clientY - draggable.pointerStart.yPos;
+            } else {
+                pointerXOffset = event.clientX - draggable.pointerStart.xPos;
+                pointerYOffset = event.clientY - draggable.pointerStart.yPos;
+            }
+            let pointerDirAngle = Math.atan2(pointerYOffset, pointerXOffset);
+            pointerDirAngle *= 180 / Math.PI;
+
+            draggable.current.xOffset =
+                draggable.previous.xOffset + pointerXOffset;
+            draggable.current.yOffset =
+                draggable.previous.yOffset + pointerYOffset;
+            draggable.current.rotation =
+                draggable.previous.rotation + pointerDirAngle;
+
+            setTransform(
+                element,
+                draggable.current.xOffset,
+                draggable.current.yOffset,
+                draggable.current.rotation
+            );
+        }
+    }
+}
+
+function dragEnd() {
+    const element = globalTargetElement;
+    const draggable = element.dragProperties;
+
+    if (draggable) {
+        const dragMotionPerformed =
+            draggable.active &&
+            draggable.current.xOffset &&
+            draggable.current.yOffset;
+
+        if (dragMotionPerformed) {
+            draggable.previous.xOffset = draggable.current.xOffset;
+            draggable.previous.yOffset = draggable.current.yOffset;
+            draggable.active = false;
+            draggable.pointerStart.xPos = null;
+            draggable.pointerStart.yPos = null;
+        }
+    }
+}
 
 function makeElementDraggable(element, initialRotation = 0) {
     element.dragProperties = {
@@ -28,74 +108,18 @@ function makeElementDraggable(element, initialRotation = 0) {
     };
 }
 
-function dragStart(event) {
-    const element = event.target;
-    const draggable = element.dragProperties;
+function waitDragEvent(dragSpace) {
+    const airplane = document.querySelector("#airplane");
+    makeElementDraggable(airplane, 45);
 
-    if (draggable) {
-        draggable.active = true;
-        draggable.pointerStart.xPos = event.clientX;
-        draggable.pointerStart.yPos = event.clientY;
+    dragSpace.addEventListener("mousedown", dragStart, false);
+    dragSpace.addEventListener("mousemove", dragMotion, false);
+    dragSpace.addEventListener("mouseup", dragEnd, false);
 
-        targetElement = element;
-    }
+    dragSpace.addEventListener("touchstart", dragStart, false);
+    dragSpace.addEventListener("touchmove", dragMotion, false);
+    dragSpace.addEventListener("touchend", dragEnd, false);
 }
 
-function dragEnd() {
-    const element = targetElement;
-    const draggable = element.dragProperties;
-
-    if (draggable) {
-        const dragMotionPerformed =
-            draggable.active &&
-            draggable.current.xOffset &&
-            draggable.current.yOffset;
-
-        if (dragMotionPerformed) {
-            draggable.previous.xOffset = draggable.current.xOffset;
-            draggable.previous.yOffset = draggable.current.yOffset;
-            draggable.active = false;
-            draggable.pointerStart.xPos = null;
-            draggable.pointerStart.yPos = null;
-        }
-    }
-}
-
-function dragMotion(event) {
-    const element = event.target;
-    const draggable = element.dragProperties;
-
-    if (draggable) {
-        const dragMotionActivated =
-            draggable.active &&
-            draggable.pointerStart.xPos &&
-            draggable.pointerStart.yPos;
-
-        if (dragMotionActivated) {
-            event.preventDefault();
-
-            let pointerXOffset = event.clientX - draggable.pointerStart.xPos;
-            let pointerYOffset = event.clientY - draggable.pointerStart.yPos;
-            let pointerDirAngle = Math.atan2(pointerYOffset, pointerXOffset);
-            pointerDirAngle *= 180 / Math.PI;
-
-            draggable.current.xOffset =
-                draggable.previous.xOffset + pointerXOffset;
-            draggable.current.yOffset =
-                draggable.previous.yOffset + pointerYOffset;
-            draggable.current.rotation =
-                draggable.previous.rotation + pointerDirAngle;
-
-            setTransform(
-                element,
-                draggable.current.xOffset,
-                draggable.current.yOffset,
-                draggable.current.rotation
-            );
-        }
-    }
-}
-
-function setTransform(element, xPos = 0, yPos = 0, angle = 0) {
-    element.style.transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
-}
+// Call main function:
+waitDragEvent(dragSpaceTarget);
